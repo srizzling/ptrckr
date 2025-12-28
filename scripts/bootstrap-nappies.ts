e541b02ae1ca180d52624da1e956a3b1b664c7b7
Sriram Venkatesh <venksriram@gmail.com>
2025-12-29 10:23:40 +1100

    fix(scripts): 🐛 correct product names and URLs
    Updated bootstrap with correct Huggies Skin Protect names and URLs from
    nappy-urls.txt

--- scripts/bootstrap-nappies.ts
+++ scripts/bootstrap-nappies.ts
@@ -24,51 +24,62 @@ interface ProductConfig {
   }[];
 }
 
-// Product configurations with all retailer URLs
+// Product configurations from nappy-urls.txt
 const PRODUCTS: ProductConfig[] = [
-  // Huggies Ultimate Newborn
+  // Huggies Skin Protect 224 pack (Costco exclusive)
   {
-    name: 'Huggies Ultimate Newborn Size 1 - 224pk',
-    imageUrl: 'https://www.bigw.com.au/medias/sys_master/images/images/h8d/h71/98071410180126.jpg',
+    name: 'Huggies Skin Protect Newborn Size 1 - 224pk',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
     scrapers: [
-      { retailer: 'Costco', url: 'https://www.costco.com.au/Baby-Kids-Toys/Nappies-Wipes-Training-Pants/Nappies/Huggies-Ultimate-Nappies-Size-1-Newborn-224-Nappies/p/1766911' }
+      { retailer: 'Costco', url: 'https://www.costco.com.au/Baby-Kids-Toys/Baby/Nappies-Wipes/Huggies-Unisex-Ultimate-Nappies-Size-1-Newborn-Up-to-5-kg-224-Nappies/p/41926' }
     ]
   },
+  // Huggies Skin Protect 160 pack
   {
-    name: 'Huggies Ultimate Newborn Size 1 - 160pk',
-    imageUrl: 'https://www.bigw.com.au/medias/sys_master/images/images/h8d/h71/98071410180126.jpg',
+    name: 'Huggies Skin Protect Newborn Size 1 - 160pk',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
     scrapers: [
-      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-ultimate-nappies-newborn-size-1-160-pack-8710170' },
-      { retailer: 'Woolworths', url: 'https://www.woolworths.com.au/shop/productdetails/842251/huggies-ultimate-nappies-newborn-size-1' },
-      { retailer: 'Big W', url: 'https://www.bigw.com.au/product/huggies-newborn-nappies-size-1-up-to-5kg-160-pack/p/52815' }
+      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-newborn-nappies-size-1-(up-to-5kg)-160-pack-4315353' },
+      { retailer: 'Big W', url: 'https://www.bigw.com.au/product/huggies-newborn-nappies-size-1-up-to-5kg-160-pack/p/52815' },
+      { retailer: 'Baby Bunting', url: 'https://www.babybunting.com.au/product/huggies-nappies-mega-newborn-size-1-160-pack-106593' }
     ]
   },
+  // Huggies Skin Protect 108 pack
   {
-    name: 'Huggies Ultimate Newborn Size 1 - 108pk',
-    imageUrl: 'https://www.bigw.com.au/medias/sys_master/images/images/h8d/h71/98071410180126.jpg',
+    name: 'Huggies Skin Protect Newborn Size 1 - 108pk',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
     scrapers: [
-      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-ultimate-nappies-newborn-size-1-108-pack-2461056' },
-      { retailer: 'Woolworths', url: 'https://www.woolworths.com.au/shop/productdetails/51461/huggies-ultimate-nappies-newborn-size-1' },
+      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-skin-protect-newborn-nappies-size-1-108-pack-9088598' },
       { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/85821/huggies-jumbo-ultimate-newborn-108-pack' },
       { retailer: 'Big W', url: 'https://www.bigw.com.au/product/huggies-newborn-nappies-size-1-up-to-5kg-108-pack/p/568518' }
     ]
   },
+  // Huggies Skin Protect 54 pack
   {
-    name: 'Huggies Ultimate Newborn Size 1 - 54pk',
-    imageUrl: 'https://www.bigw.com.au/medias/sys_master/images/images/h8d/h71/98071410180126.jpg',
+    name: 'Huggies Skin Protect Newborn Size 1 - 54pk',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
     scrapers: [
-      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-ultimate-nappies-newborn-size-1-54-pack-4758811' },
-      { retailer: 'Woolworths', url: 'https://www.woolworths.com.au/shop/productdetails/78583/huggies-ultimate-nappies-newborn-size-1' },
-      { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/57478/huggies-ultimate-newborn-54-pack' },
+      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-skin-protect-newborn-nappies-size-1-54-pack-7259907' },
+      { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/90273/huggies-ultimate-nappies-size-1-newborn-5kg-bulk-54-pack' },
       { retailer: 'Big W', url: 'https://www.bigw.com.au/product/huggies-newborn-nappies-size-1-up-to-5kg-54-pack/p/569376' }
     ]
   },
-  // Huggies Pure & Sensitive (formerly Skin Protect)
+  // Huggies Skin Protect 28 pack (convenience)
+  {
+    name: 'Huggies Skin Protect Newborn Size 1 - 28pk',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
+    scrapers: [
+      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-convenience-nappies-size-1-28-pack-7019674' },
+      { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/145563/huggies-convenience-pack-ultimate-size-1-newborn-28-pack' }
+    ]
+  },
+  // Huggies Pure & Sensitive 86 pack
   {
     name: 'Huggies Pure & Sensitive Newborn Size 1 - 86pk',
-    imageUrl: 'https://www.bigw.com.au/medias/sys_master/images/images/h8d/h71/98071410180126.jpg',
+    imageUrl: 'https://www.costco.com.au/medias/sys_master/images/h98/h56/171744033701918.jpg',
     scrapers: [
-      { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/119050/huggies-pure-sensitive-nappies-size-1-86-pack' },
+      { retailer: 'Coles', url: 'https://www.coles.com.au/product/huggies-pure-and-sensitive-newborn-nappies-size-1-86-pack-9263348' },
+      { retailer: 'Chemist Warehouse', url: 'https://www.chemistwarehouse.com.au/buy/156051/huggies-pure-and-sensitive-nappies-size-1-up-to-5kg-86-pack' },
       { retailer: 'Big W', url: 'https://www.bigw.com.au/product/huggies-pure-sensitive-nappies-size-1-86-pack/p/6018664' }
     ]
   }
