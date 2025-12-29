c074acff86c5212ed8bca17e565f60533ae06545
Sriram Venkatesh <venksriram@gmail.com>
2025-12-29 13:09:14 +1100

    feat(drizzle): ✨ add unit pricing columns
    Adds unit_count, unit_type, and price_per_unit columns to price_records
    table.

---
+++ drizzle/0009_mushy_pretty_boy.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `price_records` ADD `unit_count` integer;--> statement-breakpoint
+ALTER TABLE `price_records` ADD `unit_type` text;--> statement-breakpoint
+ALTER TABLE `price_records` ADD `price_per_unit` real;
